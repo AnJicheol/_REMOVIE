b65fabe236fa12383db74b993e9d1807b8bb171d
AnJicheol <90523400+AnJicheol@users.noreply.github.com>
2025-03-17 08:22:27 +0900

    Update RemovieReadServerApplication.java

--- backend/src/main/java/com/example/removie_read_server/RemovieReadServerApplication.java
+++ backend/src/main/java/com/example/removie_read_server/RemovieReadServerApplication.java
@@ -10,10 +10,6 @@ import org.springframework.cache.annotation.EnableCaching;
 public class RemovieReadServerApplication {
 
     public static void main(String[] args) {
-
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ALLOWED_IP: " + System.getenv("DLRJTEHDKSEHLSIQUDTLSRKXDMSTORRLDI"));
-        System.out.println("allowed.ip (Spring): " + System.getProperty("SPRING_JPA_PROPERTIES_HIBERNATE_FORMAT_SQL"));
-
         SpringApplication.run(RemovieReadServerApplication.class, args);
     }
 
